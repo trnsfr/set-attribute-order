eed10dec4a0e426f27834fdacd0ba52f32377a31
Brian Lonsdorf <brian@trnsfr.com>
2008-12-05 11:24:20 -0600

    only sets attributes you pass in, then calls super

--- set_attribute_order.rb
+++ set_attribute_order.rb
@@ -16,9 +16,8 @@ module SetAttributeOrder
   def initialize(attrs={})
     order = self.class.attribute_ordered_array
     return super if order.empty?
-    ordered = attrs.sort_by {|k, v| order.index(k.to_sym) || 0 }
-    ordered.each{ |k,v| self.send(:"#{k}=", v) }
-    attrs.reject!{ |k,v| order.include?(k.to_sym) }
+    setters = attrs.inject({}){ |h,(k,v)| h[k]=attrs.delete(k) if order.include?(k.to_sym) ; h }
+    setters.sort_by{ |k,v| order.index(k.to_sym) || setters.length}.each{ |k,v| self.send(:"#{k}=", v)}
     super
   end
   
